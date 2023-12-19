4cc3153817c088ab796c4a222edbcf5555a3ad12
Кирилл Попов <diplomatgmg@gmail.com>
2023-12-19 12:40:04 +0300

    Улучшена логика программы
    Добавлена возможность сыграть повторно

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const moveMapping = {
   3: moves.scissors,
 };
 
-const getUserAnswer = () => readlineSync.question('\nInput your move\n1. Rock\n2. Paper\n3. Scissors\n:');
+const getUserInput = (message = '') => readlineSync.question(message);
 
 const getMove = (move) => {
   const formattedMove = move.trim().toLowerCase();
@@ -28,27 +28,39 @@ const getMove = (move) => {
   return moveMapping[formattedMove] || null;
 };
 
-const getRandomMove = () => {
+const getComputerMove = () => {
   const randomNumber = Math.floor(Math.random() * 3) + 1;
   return moveMapping[randomNumber.toString()];
 };
 
-const userIsWinner = (userMove, randomMove) => {
+const userIsWinner = (userMove, computerMove) => {
   const winMapper = {
     [moves.rock]: moves.scissors,
     [moves.paper]: moves.rock,
     [moves.scissors]: moves.paper,
   };
 
-  if (userMove === randomMove) {
+  if (userMove === computerMove) {
     return null; // Draw
   }
 
-  return winMapper[userMove] === randomMove; // True if user wins
+  return winMapper[userMove] === computerMove; // True if user wins
+};
+
+const isYes = (string) => {
+  switch (string.toLowerCase()) {
+    case 'yes':
+    case 'y':
+      return true;
+    default:
+      return false;
+  }
 };
 
 const game = () => {
-  const userAnswer = getUserAnswer();
+  console.log('\nInput your move\n1. Rock\n2. Paper\n3. Scissors');
+
+  const userAnswer = getUserInput('\nYour choice: ');
   const userMove = getMove(userAnswer);
 
   if (!userMove) {
@@ -57,26 +69,38 @@ const game = () => {
     return;
   }
 
-  const randomMove = getMove(getRandomMove().toString());
+  console.log(`You have chosen: ${userMove}`);
 
-  const result = userIsWinner(userMove, randomMove);
+  const computerMove = getMove(getComputerMove().toString());
 
-  switch (result) {
+  console.log(`\nThe computer chooses: ${computerMove}`);
+
+  const isUserWin = userIsWinner(userMove, computerMove);
+
+  switch (isUserWin) {
     case true:
-      console.log(`Congratulations! You've won with "${userMove}" against "${randomMove}"!`);
+      console.log(`\nResult: Congratulations! You've won with "${userMove}" against "${computerMove}"!`);
       break;
     case null:
-      console.log(`Not bad, it's a draw. You both chose "${userMove}".`);
+      console.log(`\nResult: Not bad, it's a draw. You both chose "${userMove}".`);
       break;
     default:
-      console.log(`You've lost. Your "${userMove}" is defeated by "${randomMove}". Try again :)`);
+      console.log(`\nResult: You've lost. Your "${userMove}" is defeated by "${computerMove}". Try again :)`);
       break;
+  }
+
+  const playAgainUserInput = getUserInput('\nDo you want to play again? (yes/no): ');
+  const playAgain = isYes(playAgainUserInput);
+
+  if (playAgain) {
+    game();
   }
 };
 
 const run = () => {
   console.log('Welcome to the game "Rock Paper Scissors"!');
   game();
+  console.log('\nThanks for playing! See you!');
 };
 
 run();
